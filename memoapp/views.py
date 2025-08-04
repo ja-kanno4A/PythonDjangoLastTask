e51d66ad0d0f6e54e5cb5534e265223af40541e9
konnnoairi <a.konno.sys24@morijyobi.ac.jp>
2025-08-04 14:03:32 +0900

    右メニューにgitbash追加,2025/08/04の開始

--- memoapp/views.py
+++ memoapp/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render,redirect
-from django.views import View # クラスベースビューを継承するのに必要
+from .forms import MemoAddForm,TagsAddForm
 from django.urls import reverse_lazy 
+from .models import Memos,Tags
+from django.views import View
+
 class IndexView(View):
     def get(self,req):
         return render(req,"memoApp/index.html")
     
 class AddPageView(View):
     def get(self,req):
-        return render(req,"memoApp/addpage.html")
+        form = MemoAddForm()
+        return render(req,"memoApp/addpage.html",{"form":form})
+
+class TagAddView(View):
+    def get(self,req):
+        return render(req,"")
     
 memoindex = IndexView.as_view()
 add = AddPageView.as_view()
